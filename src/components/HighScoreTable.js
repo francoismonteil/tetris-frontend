6a07840c80a6ab27c7b2679bba44cb2d80041761
francoismonteil <francois.monteil@protonmail.com>
2024-06-02 14:31:51 +0200

    feat: better scores table

--- src/components/HighScoreTable.js
+++ src/components/HighScoreTable.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { FixedSizeList as List } from 'react-window';
 import './../css/HighScoreTable.css';
 import { motion } from 'framer-motion';
 
@@ -23,6 +24,16 @@ function HighScoreTable({ onReplay }) {
         fetchScores();
     }, []);
 
+    const Row = ({ index, style }) => (
+        <div style={style} className="table-row-container">
+            <div className="table-row">
+                <div className="table-cell">{index + 1}</div>
+                <div className="table-cell">{scores[index].name}</div>
+                <div className="table-cell">{scores[index].score}</div>
+            </div>
+        </div>
+    );
+
     return (
         <motion.div
             id="high-score-table"
@@ -30,25 +41,26 @@ function HighScoreTable({ onReplay }) {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5 }}
         >
-            <h2>High Scores</h2>
-            <table>
-                <thead>
-                <tr>
-                    <th>Rank</th>
-                    <th>Name</th>
-                    <th>Score</th>
-                </tr>
-                </thead>
-                <tbody>
-                {scores.map((score, index) => (
-                    <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{score.name}</td>
-                        <td>{score.score}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
+            <h2 className="high-score-title">High Scores</h2>
+            <div className="table-container">
+                <div className="table-header">
+                    <div className="table-row">
+                        <div className="table-cell">Rank</div>
+                        <div className="table-cell">Name</div>
+                        <div className="table-cell">Score</div>
+                    </div>
+                </div>
+                <div className="table-body">
+                    <List
+                        height={400} // Adjust the height as needed
+                        itemCount={scores.length}
+                        itemSize={50} // Adjust the size as needed
+                        width="100%"
+                    >
+                        {Row}
+                    </List>
+                </div>
+            </div>
             <motion.button className="replay-button" onClick={onReplay} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
                 Rejouer
             </motion.button>
